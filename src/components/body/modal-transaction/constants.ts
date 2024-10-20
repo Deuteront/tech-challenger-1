b00dfe7088d076a707cf7ac454a437e8e81a3516
deuteront <paulo.santos.h@outlook.com>
2024-10-20 17:49:46 -0300

    adjust

--- src/components/body/modal-transaction/constants.ts
+++ src/components/body/modal-transaction/constants.ts
@@ -1,50 +1,49 @@
 import { transaction } from '@/components/body/modal-transaction/modal-transaction.type';
 import { getFromStorage, saveToStorage } from '@/utils/storage';
 
-
 export const transactionsName = 'transactions';
 
 export const initialTransactionData: transaction = {
-    value: '',
-    movement: '',
-    paymentMethod: '',
-    establishmentType: '',
-    transactionDate: null,
-  };
+  value: '',
+  movement: '',
+  paymentMethod: '',
+  establishmentType: '',
+  transactionDate: null,
+};
 
-  export const handleNext = (
-    step: number,
-    setStep: (step: number) => void,
-    transactionData: transaction,
-    closeModal: () => void
-  ) => {
-    if (step < 2) {
-      setStep(step + 1);
-    } else {
-      const transactionKey = transactionsName;
-      const storedTransactions =
-        (getFromStorage(transactionKey) as transaction[]) || [];
-      
-        const newTransaction = {
-          ...transactionData,
-          id: generateTransactionId(),
-        };
+export const handleNext = (
+  step: number,
+  setStep: (step: number) => void,
+  transactionData: transaction,
+  closeModal: () => void
+) => {
+  if (step < 2) {
+    setStep(step + 1);
+  } else {
+    const transactionKey = transactionsName;
+    const storedTransactions =
+      (getFromStorage(transactionKey) as transaction[]) || [];
 
-      storedTransactions.push(newTransaction);
-      saveToStorage(transactionKey, storedTransactions);
-      closeModal();
-    }
-  };
+    const newTransaction = {
+      ...transactionData,
+      id: generateTransactionId(),
+    };
 
-  export const handlePrev = (step: number, setStep: (step: number) => void) => {
-    if (step > 0) {
-      setStep(step - 1);
-    }
-  };
+    storedTransactions.push(newTransaction);
+    saveToStorage(transactionKey, storedTransactions);
+    closeModal();
+  }
+};
 
-  export const generateTransactionId = (): number => {
-    const currentId = getFromStorage('transactionIdCounter') as number || 0;
-    const newId = currentId + 1;
-    saveToStorage('transactionIdCounter', newId);
-    return newId;
-  };
+export const handlePrev = (step: number, setStep: (step: number) => void) => {
+  if (step > 0) {
+    setStep(step - 1);
+  }
+};
+
+export const generateTransactionId = (): number => {
+  const currentId = (getFromStorage('transactionIdCounter') as number) || 0;
+  const newId = currentId + 1;
+  saveToStorage('transactionIdCounter', newId);
+  return newId;
+};
